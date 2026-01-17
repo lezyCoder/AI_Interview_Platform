d9853d0c1a041f12974446b0fea33ea2809aa017
lezyCoder <iamvijaysingh23@gmail.com>
2026-01-17 18:22:52 +0530

    Update package dependencies and refactor ChatScreen and Sidebar components for improved structure and styling

--- src/components/ChatScreen.jsx
+++ src/components/ChatScreen.jsx
@@ -1,44 +1,55 @@
-import { useState } from "react"
-import Navbar from "./Navbar"
+import { useState } from "react";
+import Navbar from "./Navbar";
 
 const ChatScreen = () => {
+  const [answer, setAnswer] = useState("");
 
-    const [answer, setAnswer] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
-    
-    return (
-        <div className="flex flex-col flex-1 ">
-            <Navbar />
+  return (
+    <div className="flex flex-col flex-1 ">
+      <Navbar />
 
-            {/* Chat area */}
-            <div className="flex-1 overflow-y-auto ">
-                <div className="bg-gray-800 min-h-full w-full p-2">
-                    Lorem ipsum...
-                </div>
+      {/* Chat area */}
+      <div className="flex-1 overflow-y-auto  ">
+        <div className="chat-screen bg-gray-800 min-h-full w-full p-2">
+          <div className="chat-container p-2">
+            <div className="chat-bubbles flex flex-col gap-y-4">
+              <div className="ai flex justify-start w-full border border-gray-700 rounded   ">
+                <p className="p-2">Hello</p>
+              </div>
+              <div className="user flex justify-end border border-gray-700 rounded   ">
+                <p className="p-2 ">Hello</p>{" "}
+              </div>
+              <div className="ai flex justify-start w-full">
+                <p className="p-2 w-fit  rounded  ">How are you ?</p>
+              </div>
+              <div className="user flex justify-end w-full">
+                <p>I am good </p>
+              </div>
             </div>
-
-            {/* Input bar */}
-    
-            <div className="bg-base-500 p-2">
-                <div className="flex justify-center">
-                    <form className="flex items-center gap-2 w-full max-w-2xl">
-                        <textarea
-                            className="flex-1 resize-none outline-none border rounded-full px-4 py-2"
-                            rows={1}
-                            placeholder="Ask Anything"
-                        />
-                        <button className="px-4 py-2 border rounded-full">
-                            Send
-                        </button>
-                    </form>
-                </div>
-            </div>
-
+          </div>
         </div>
-    )
-}
+      </div>
 
-export default ChatScreen
+      {/* Input bar */}
+
+      <div className="bg-base-500 p-2">
+        <div className="flex justify-center">
+          <form className="flex items-center gap-2 w-full max-w-2xl">
+            <textarea
+              className="flex-1 resize-none outline-none border rounded-full px-4 py-2"
+              rows={1}
+              placeholder="Ask Anything"
+            />
+            <button className="px-4 py-2 border rounded-full">Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatScreen;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 import { FaGear } from "react-icons/fa6";
 import { RiChatNewLine } from "react-icons/ri";
 import { VscNewFolder } from "react-icons/vsc";
@@ -7,23 +7,27 @@ import { IoSearch } from "react-icons/io5";
 import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Sidebar = () => {
-  const [isOpen, setOpen] = useState(true)
+  const [isOpen, setOpen] = useState(true);
 
   const MenuItem = ({ icon: Icon, label }) => {
     return (
-      <button className={`flex text-center items-center gap-4  w-full font-thin text-xl py-2 hover:bg-gray-500 rounded`}>
+      <button
+        className={`flex text-center items-center gap-4  w-full font-thin text-xl py-2 hover:bg-gray-500 rounded`}>
         <Icon icon={RiChatNewLine} className="text-2xl" />
         {isOpen && <span>{label}</span>}
       </button>
-    )
-  }
+    );
+  };
   return (
-
-    <aside >
-      <div className={`${isOpen ? "lg:w-48" : "lg:w-14 "} md:w-36 flex flex-col items-center justify-between bg-base-500 h-full p-4 gap-2 border border-gray-700`} onClick={() => setOpen(!isOpen)}>
-        <div className="navigation flex justify-between items-center w-full" >
+    <aside>
+      <div
+        className={`${isOpen ? "lg:w-64" : "lg:w-14 "} md:w-36 flex flex-col items-center justify-between bg-base-500 h-full p-4 gap-2 border border-gray-700`}
+        onClick={() => setOpen(!isOpen)}>
+        <div className="navigation flex justify-between items-center w-full">
           <h1>AI</h1>
-          <button className={`${isOpen ? "block" : "hidden"}`} onClick={() => setOpen(!isOpen)} >
+          <button
+            className={`${isOpen ? "block" : "hidden"}`}
+            onClick={() => setOpen(!isOpen)}>
             <BsLayoutSidebarReverse />
           </button>
         </div>
@@ -37,12 +41,20 @@ const Sidebar = () => {
 
           {isOpen && (
             <>
-              <div className="flex justify-between items-center text-gray-500 text-sm py-2" onClick={(e) => { e.stopPropagation() }}>
+              <div
+                className="flex justify-between items-center text-gray-500 text-sm py-2"
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}>
                 <p>Projects</p>
                 <RiArrowDropDownLine className="text-xl" />
               </div>
 
-              <div className="flex justify-between items-center text-gray-500 text-sm py-2" onClick={(e) => { e.stopPropagation() }}>
+              <div
+                className="flex justify-between items-center text-gray-500 text-sm py-2"
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}>
                 <p>Chats</p>
                 <RiArrowDropDownLine className="text-xl" />
               </div>
@@ -57,11 +69,8 @@ const Sidebar = () => {
           <MenuItem icon={FaGear} label="Settings" />
         </div>
       </div>
-    </aside >
+    </aside>
+  );
+};
 
-  )
-}
-
-
-
-export default Sidebar
+export default Sidebar;
